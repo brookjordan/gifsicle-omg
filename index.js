872f4ac64baf7642715f84bbcdfb8eec5ebe2433
Brook Jordan <brkjrdn@gmail.com>
2020-02-08 18:35:21 +0800

    Fully featured

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const fs = require("fs");
 const path = require("path");
+const { promisify } = require("util");
 const multer = require("multer");
-const { execFile } = require("child_process");
+const childProcess = require("child_process");
 const gifsicle = require("gifsicle");
+const stat = promisify(fs.stat);
+const unlink = promisify(fs.unlink);
+const exec = promisify(childProcess.exec);
+const execFile = promisify(childProcess.execFile);
+
 const app = require("express")();
 
 let uploadGif = multer({ dest: "i" })
 
 function sendGif(request, resource, next) {
-  let filePath = path.join(__dirname, request.global.gifPath);
+  let filePath = path.join(__dirname, "i", request.params.src);
   let fileType = "image/gif";
   let fileStream = fs.createReadStream(filePath);
 
   fileStream.on("open", () => {
     resource.set("Content-Type", fileType);
     fileStream.pipe(resource);
-    fs.unlink(request.global.gifPath, _=>_);
-    fs.unlink(request.global.gifPath.split('.')[0], _=>_);
   });
 
   fileStream.on("error", () => {
     resource.set("Content-Type", "text/plain");
     resource.status(404);
     resource.send("Not found");
-    fs.unlink(request.global.gifPath, _=>_);
-    fs.unlink(request.global.gifPat.split('.')[0], _=>_);
   });
 };
+
+let killedOnce = false;
+function shutdown(signal) {
+  return async err => {
+    console.log(signal);
+    if (killedOnce) { return process.exit(1); }
+    console.log("Tidying up. Kill again to kill instantly.");
+    killedOnce = true;
+
+    if (err) console.error(err.stack || err);
+    let killPromises = fs.readdirSync("i").map(filePath => unlink(path.join("i", filePath)));
+    await Promise.all(killPromises);
+    process.exit(err ? 1 : 0);
+  };
+}
+process
+  .on("SIGTERM", shutdown("SIGTERM"))
+  .on("SIGINT", shutdown("SIGINT"))
+  .on("uncaughtException", shutdown("uncaughtException"));
 
 if (!fs.existsSync("i")){ fs.mkdirSync("i"); }
 
@@ -37,28 +58,98 @@ app.get("/", (request, resource, next) => {
   resource.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/i/:src",
+  sendGif,
+);
+
+const fileMetadata = require('file-metadata');
 app.post("/optimise",
   uploadGif.single("gif"),
-  (request, resource, next) => {
+  async (request, resource, next) => {
     let compressedGifPath = `${request.file.path}.gif`;
-    let options = [`--output=${compressedGifPath}`];
-    if (+request.body.lossy) {
-      options.push(`--lossy=${Math.round(request.body.lossy)}`);
+    let framesWereDeleted = false;
+    let options = [
+      `--output=${compressedGifPath}`,
+    ];
+    if (request.body.delete && request.body.delete !== "none") {
+      let frameCount = (await exec(`identify -format "%n\n" ${request.file.path} | head -n 1`)).stdout.trim();
+      let allFrames = Array.from({ length: frameCount }, (_, i) => i);
+      let frames;
+      switch (request.body.delete) {
+        case "1/4":
+          frames = allFrames.filter(i => i % 4 === 3);
+          break;
+        case "1/3":
+          frames = allFrames.filter(i => i % 3 === 2);
+          break;
+        case "1/2":
+          frames = allFrames.filter(i => i % 2 === 1);
+          break;
+        case "2/3":
+          frames = allFrames.filter(i => i % 3 !== 0);
+          break;
+        case "3/4":
+          frames = allFrames.filter(i => i % 4 !== 0);
+          break;
+      }
+      if (frames && frames.length) {
+        framesWereDeleted = true;
+        options.push(
+          request.file.path,
+          "--delete",
+          ...frames.map(a => `#${a}`),
+          "--done",
+        );
+      }
     }
-    if (
-      request.body.optimize
-      && request.body.optimize > 0 && request.body.optimize < 4
-    ) {
-      options.push(`--optimize=${Math.round(request.body.optimize)}`);
+
+    // The following don’t work if frames are removed
+    if (!framesWereDeleted) {
+      [
+        "flip-horizontal",
+        "flip-vertical",
+        "crop-transparency",
+      ].forEach(booleans => {
+        if (request.body[booleans]) {
+          options.push(`--${booleans}`);
+        }
+      });
+      if (request.body.rotate && +request.body.rotate) {
+        options.push(`--rotate-${request.body.rotate}`);
+      }
+      if (request.body.crop_method) {
+        let method = request.body.crop_method;
+        let fromX = request.body.crop_x_from || 0;
+        let toX = request.body.crop_x_to || 0;
+        let fromY = request.body.crop_y_from || 0;
+        let toY = request.body.crop_y_to || 0;
+
+        if (fromX || toX || fromY || toY) {
+          if (method === "trim") {
+            options.push(`--crop=${fromX},${fromY}+-${toX}x-${toY}`);
+          } else if (method === "to") {
+            options.push(`--crop=${fromX},${fromY}-${toX},${toY}`);
+          } else if (method === "size") {
+            options.push(`--crop=${fromX},${fromY}+${toX}x${toY}`);
+          }
+        }
+      }
     }
+
+    if (+request.body.resize_colors || +request.body.resize_colors) {
+      options.push(`--resize-colors=${+request.body.resize_colors}`);
+    }
+    [
+      "conserve-memory",
+      "no-conserve-memory",
+      "interlace",
+    ].forEach(booleans => {
+      if (request.body[booleans]) {
+        options.push(`--${booleans}`);
+      }
+    });
     if (request.body.colors && +request.body.colors) {
       options.push(`--colors=${Math.round(request.body.colors)}`);
-    }
-    if (request.body.flip_horizontal && request.body.flip_horizontal.toLowerCase() === "true") {
-      options.push("--flip-horizontal");
-    }
-    if (request.body.flip_vertical && request.body.flip_vertical.toLowerCase() === "true") {
-      options.push("--flip-vertical");
     }
     if (+request.body.resize_x || +request.body.resize_y) {
       let resizeX = +request.body.resize_x;
@@ -73,25 +164,58 @@ app.post("/optimise",
         options.push(`--scale=${Math.min(2, resizeX)}x${Math.min(2, resizeY)}`);
       }
     }
-    if (+request.body.resize_colors || +request.body.resize_colors) {
-      options.push(`--resize-colors=${+request.body.resize_colors}`);
-    }
     if (
       request.body.color_method
       && ["diversity", "blend-diversity", "median-cut"].includes(request.body.color_method)
     ) {
       options.push(`--color-method=${request.body.color_method}`);
     }
-    options.push(request.file.path);
-    console.log(options);
+    if (request.body.dither) {
+      options.push(`--dither=${request.body.dither}`);
+    }
+    if (+request.body.lossy) {
+      options.push(`--lossy=${Math.round(request.body.lossy)}`);
+    }
+    if (
+      request.body.optimize
+      && request.body.optimize > 0 && request.body.optimize < 4
+    ) {
+      options.push(`--optimize=${Math.round(request.body.optimize)}`);
+    }
+    if (!framesWereDeleted) {
+      options.push(request.file.path);
+    }
 
-    execFile(gifsicle, options, error => {
-      if (error) { throw new Error(error); }
-      (request.global || (request.global = {})).gifPath = compressedGifPath;
-      next();
+    try {
+      await execFile(gifsicle, options);
+    } catch (error) {
+      throw new Error(error);
+    }
+
+    let [
+      originalGifStats,
+      optimisedGifStats,
+    ] = await Promise.all([
+      stat(request.file.path),
+      stat(compressedGifPath),
+    ])
+
+    console.log(`Original size: ${originalGifStats.size}`);
+    console.log(`Optimised size: ${optimisedGifStats.size}`);
+
+    resource.status(200);
+    resource.send({
+      src: compressedGifPath,
+      originalSize: originalGifStats.size,
+      optimisedSize: optimisedGifStats.size,
+      requestID: request.body.request_id,
     });
+
+    setTimeout(() => {
+      unlink(request.file.path, e => e);
+      unlink(compressedGifPath, e => e);
+    }, 60 * 1000);
   },
-  sendGif,
 );
 
 app.get("*", (request, resource, next) => {
